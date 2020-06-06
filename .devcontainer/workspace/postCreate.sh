7162cfaf0ca37c78c109f83220ce4d837bee2862
Ryan Dowling <ryan@ryandowling.me>
2020-06-06 04:16:44 +0000

    fix: devcontainer permissions and remove .ssh mapping

--- .devcontainer/workspace/postCreate.sh
+++ .devcontainer/workspace/postCreate.sh
@@ -3,19 +3,12 @@
 # change owner to vscode user for mapped files/directories
 sudo chown vscode node_modules
 sudo chown vscode /home/vscode/history
-sudo chown -R vscode /home/vscode/.ssh
-sudo chown vscode /home/vscode/.gnupg/pubring.kbx
-sudo chown vscode /home/vscode/.gnupg/trustdb.gpg
-sudo chown -R vscode /home/vscode/.gnupg/private-keys-v1.d
-sudo chown vscode /home/vscode/.gnupg-localhost/S.gpg-agent.extra
+sudo chown -R vscode /home/vscode/.gnupg
 
 # fix file permissions in /home/vscode/.gnupg folder
+chmod 700 /home/vscode/.gnupg
 find /home/vscode/.gnupg -type f -exec chmod 600 {} \\;
 find /home/vscode/.gnupg -type d -exec chmod 700 {} \\;
-
-# fix file permissions in /home/vscode/.ssh folder
-find /home/vscode/.ssh -type f -exec chmod 600 {} \\;
-find /home/vscode/.ssh -type d -exec chmod 700 {} \\;
 
 # add in GPG_TTY to bashrc to allow VSCode to sign within the UI
 echo 'export GPG_TTY=$(tty)' >> /home/vscode/.bashrc
